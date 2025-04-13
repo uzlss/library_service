a3713fe6083ba047ba49a97ca15c59e89acffd3c
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-14 01:29:57 +0300

    add book field to borrowing

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from books_service.models import Book
+
 
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
