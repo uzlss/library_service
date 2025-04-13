82942ad224e9be5d46b7f76df55baed91a69a383
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-14 01:33:40 +0300

    add borrowing viewset w/ list&detail endpoints

---
+++ borrowings_service/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+import books_service.serializers
+from borrowings_service.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        )
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = books_service.serializers.BookSerializer
