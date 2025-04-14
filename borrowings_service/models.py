77538c0a10674eb645ed4e254ae2db211b5a2415
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-14 18:50:37 +0300

    add auto_now to borrow_date

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -4,7 +4,7 @@ from books_service.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
