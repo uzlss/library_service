960ca533cf068b18f15d1117bbbd79d2b4b47b72
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-14 19:39:21 +0300

    update borrowing viewset

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -4,11 +4,15 @@ from borrowings_service.models import Borrowing
 from borrowings_service.serializers import (
     BorrowingDetailSerializer,
     BorrowingSerializer,
+    BorrowingCreateSerializer,
 )
 
 
 class BorrowingViewSet(
-    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
 ):
     queryset = Borrowing.objects.all().prefetch_related("book")
     serializer_class = BorrowingSerializer
@@ -16,4 +20,6 @@ class BorrowingViewSet(
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
         return self.serializer_class
