64a6bc6c67fa87b0efd69cac6eb0051a7afc285e
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-14 19:35:50 +0300

    add validation to the model

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from books_service.models import Book
@@ -8,3 +9,24 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
+
+    @staticmethod
+    def validate_borrowing(book, error_to_raise):
+        if book.inventory < 1:
+            raise error_to_raise(
+                {
+                    "inventory": (
+                        "Book inventory must be greater than or equal to 1 "
+                        "to borrow the book."
+                        f"(current inventory: {book.inventory})"
+                    )
+                }
+            )
+
+    def clean(self):
+        if self.book:
+            Borrowing.validate_borrowing(self.book, ValidationError)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
