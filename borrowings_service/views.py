f1f3a8614a3553d9507f533ba729ab7a817d9eb8
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-15 14:36:35 +0300

    refactor: new serializers in viewset

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -3,10 +3,11 @@ from rest_framework.permissions import IsAuthenticated
 
 from borrowings_service.models import Borrowing
 from borrowings_service.serializers import (
-    BorrowingDetailSerializer,
     BorrowingSerializer,
     BorrowingCreateSerializer,
+    BorrowingDetailSerializer,
     BorrowingAdminSerializer,
+    BorrowingAdminDetailSerializer,
 )
 
 
@@ -25,6 +26,8 @@ class BorrowingViewSet(
 
     def get_serializer_class(self):
         if self.request.user.is_staff:
+            if self.action == "retrieve":
+                return BorrowingAdminDetailSerializer
             if self.action == "list":
                 return BorrowingAdminSerializer
 
@@ -35,19 +38,17 @@ class BorrowingViewSet(
         return self.serializer_class
 
     def get_queryset(self):
+        user = self.request.user
         queryset = self.queryset
 
-        if self.request.user.is_staff:
+        if user.is_staff:
             user_id = self.request.query_params.get("user_id")
             if user_id:
-                queryset = Borrowing.objects.filter(user_id=user_id)
+                queryset = queryset.filter(user_id=user_id)
         else:
-            queryset = Borrowing.objects.filter(user=self.request.user)
+            queryset = queryset.filter(user=user)
 
-        is_active = self.request.query_params.get("is_active")
-        if is_active in ("true", "True", "1"):
-            queryset = Borrowing.objects.filter(
-                actual_return_date__isnull=True
-            )
+        if self.request.query_params.get("is_active") in ("true", "True", "1"):
+            queryset = queryset.filter(actual_return_date__isnull=True)
 
         return queryset.distinct()
