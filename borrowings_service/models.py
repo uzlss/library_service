cbaf4e5f784390f86046bdbc8b88daf58baa7c19
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-15 12:37:57 +0300

    Alter actual_return_date to blank=True

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -8,7 +8,7 @@ from books_service.models import Book
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField(null=True, default=None)
+    actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
