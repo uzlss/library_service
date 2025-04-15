f66500cbd8e328289fa5b616588507f5e4c82b8d
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-15 12:32:45 +0300

    add default value to actual_return_date

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -8,7 +8,7 @@ from books_service.models import Book
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, default=None)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
