ce43dafbdcc317ffa2ae3112dad6cb8477242ca3
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-17 11:46:44 +0300

    add redis settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -174,7 +174,7 @@ INTERNAL_IPS = [
 ]
 
 # Celery
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = os.getenv("REDIS_HOST", "redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
